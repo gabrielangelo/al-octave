6b648b48f85882e459b341d73c486c869148cb72
gabriel <gabri005@gmail.com>
2018-09-24 13:41:49 -0300

    added somem validations to gauss jordan elimination and gauss_eli_lu script

--- trabalho1/sistemas_lineares/gauss_eli_lu.m
+++ trabalho1/sistemas_lineares/gauss_eli_lu.m
@@ -1,12 +1,18 @@
 function [x] = gauss_eli_lu (A, b)
-	[L, U, P] = LU_pivot(A);
-	y = L_substituicao (L, P*b);
-	rank = _rank(U, y);	
-	if (rank == rows (U))
-		x = U_substituicao (U, y);
-	elseif (rank < rows (U) && rank > -1)
-		x = solve_mult_sol (U, y);
-	elseif (rank == -1)
-		disp("solução inconsistente");
+	if (!istriu(A))
+		[L, U, P] = LU_pivot(A);
+		y = L_substituicao (L, P*b);
+		rank = _rank(U, y);	
+		if (rank == rows (U))
+			disp (y);
+			x = U_substituicao (U, y);
+			
+		elseif (rank < rows (U) && rank > -1)
+			x = solve_mult_sol (U, y);
+		elseif (rank == -1)
+			disp("solução inconsistente");
+		endif
+	else
+		x = U_substituicao  (A, b);
 	endif
 endfunction
--- trabalho1/sistemas_lineares/gauss_jordan_elimination.m
+++ trabalho1/sistemas_lineares/gauss_jordan_elimination.m
@@ -1,25 +1,28 @@
+function [matrix] = gauss_jordan_elimination(matrix) 
+	if (rows(matrix) != columns(matrix))
+		disp("matriz deve ser quadrada!");
+		return 
+  	endif
 
-function [matrix] = gauss_jordan_elimination(matrix)
-
- [row,col] = size(matrix);
-
-  m = 1;
-  n = 1;
-  
-  while (m <= row) && (n <= col)
-     [j,i] = max(abs(matrix(m:row,n)));
-     i = i+m-1;
-     if (j <= 0)
-        matrix(m:row,n) = 0;
-        n = n + 1;
-     else
-        matrix([m i],n:col) = matrix([i m],n:col);
-        temp = matrix(m,n:col)/matrix(m,n);    
-        matrix(:,n:col) = matrix(:,n:col) - matrix(:,n)*temp;
-        matrix(m,n:col) = temp;
-        m = m + 1;
-        n = n + 1;
-     end
-  end
+  	matrix = [matrix eye(rows (matrix), columns (matrix))]
+  	[row,col] = size(matrix);
+  	m = 1;
+  	n = 1;
+ 
+	while (m <= row) && (n <= col)
+     		[j,i] = max(abs(matrix(m:row,n)));
+    	 	i = i+m-1;
+     		if (j <= 0)
+        		matrix(m:row,n) = 0;
+        		n = n + 1;
+     		else
+        		matrix([m i],n:col) = matrix([i m],n:col);
+        		temp = matrix(m,n:col)/matrix(m,n);    
+        		matrix(:,n:col) = matrix(:,n:col) - matrix(:,n)*temp;
+        		matrix(m,n:col) = temp;
+        		m = m + 1;
+        		n = n + 1;
+		endif
+	end
   
 endfunction
