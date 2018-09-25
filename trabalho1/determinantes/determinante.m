e916e9292a41991c2da733a43f3f08e33ee5fb41
Leonardo <leonardo.uece@gmail.com>
2018-09-25 00:03:15 -0300

    create a main function
    create a main function to run the determinant calculator

--- trabalho1/determinantes/determinante.m
+++ trabalho1/determinantes/determinante.m
@@ -2,13 +2,12 @@ function [y] = determinante(A)
 m = length(A);
 y = 0;
 
-if(m == 2)
-    y = A(1,1) * A(2,2)-A(2,1) * A(1,2);
-
-elseif size(A,1)~=size(A,2)
-    warn_msg = "matrix not square; no proper determinant";
+if size(A,1)~=size(A,2)
+    warn_msg = "essa nao eh uma matriz quadrada";
     disp(warn_msg);
 
+elseif (m == 2)
+    y = A(1,1) * A(2,2)-A(2,1) * A(1,2);
 else
     for i=1:m
         y = y+((-1) ^ (1+i)) * A(1,i) * cofator(A,1,i);
---
+++ trabalho1/determinantes/main.m
@@ -0,0 +1,7 @@
+clear all;
+clc
+close all;
+% Determinante de uma matriz
+A=input('enter the matrix');
+disp(A)
+determinante(A)
