fd21c798fbde9675cb2b62b8218b063de1ff9819
gabriel <gabri005@gmail.com>
2018-09-26 00:03:08 -0300

    count permutations matricees variable added to LU_pivot

--- trabalho1/sistemas_lineares/LU_pivot.m
+++ trabalho1/sistemas_lineares/LU_pivot.m
@@ -1,5 +1,5 @@
-function [L, U, P] = LU_pivot(A)
-    [m, n] = size(A); L=eye(n); P=eye(n); U=A; h=m;
+function [L, U, P, cont_perm] = LU_pivot(A)
+    [m, n] = size(A); L=eye(n); P=eye(n); P_aux = P; U=A; h=m; cont_perm = 0;
     if ( m < n)
 	L = eye (m);
 	P = eye (m);
@@ -16,6 +16,11 @@ function [L, U, P] = LU_pivot(A)
         U([k,ind],k:h)=U([ind,k],k:h);
         L([k,ind],1:k-1)=L([ind,k],1:k-1);
         P([k,ind],:)=P([ind,k],:);
+	if (!isequal (P_aux, P))
+		cont_perm++;
+		P_aux = P;
+	endif
+ 
         for j=k+1:m
             L(j,k)=U(j,k)/U(k,k);
             U(j,k:n)=U(j,k:h)-L(j,k)*U(k,k:h);
