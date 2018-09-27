7d0794ae2ada288c36145e4f95aa798d36c6f98b
gabriel <gabri005@gmail.com>
2018-09-26 22:55:00 -0300

    some comments added to sistemas_lineares/multiples_entries

--- trabalho1/sistemas_lineares/multiples_entries.m
+++ trabalho1/sistemas_lineares/multiples_entries.m
@@ -1,4 +1,6 @@
 function multiples_entries (A, b)
+	# basicamente uma extensão de gauss_eli_lu considerando múltiplas entradas (vetores b)
+	# https://github.com/gabrielangelo/al-octave/blob/master/trabalho1/sistemas_lineares/gauss_eli_lu.m
 	cols_b = columns (b);
 	[L, U, P] = LU_pivot (A);
 	[lin_a, cols_a] = size (A);
