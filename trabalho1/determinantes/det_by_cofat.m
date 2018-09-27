d55a4193375e775f75267351a2b2464588147aac
Leonardo <leonardo.uece@gmail.com>
2018-09-26 23:15:08 -0300

    create the comments and readme

--- trabalho1/determinantes/det_by_cofat.m
+++ trabalho1/determinantes/det_by_cofat.m
@@ -1,17 +1,16 @@
 function [y] = det_by_cofat(A)
+    m = length(A);
+    y = 0;
 
-m = length(A);
-y = 0;
+    if size(A,1)~=size(A,2)
+        warn_msg = "essa nao eh uma matriz quadrada";
+        disp(warn_msg);
 
-if size(A,1)~=size(A,2)
-    warn_msg = "essa nao eh uma matriz quadrada";
-    disp(warn_msg);
-
-elseif (m == 2)
-    y = A(1,1) * A(2,2)-A(2,1) * A(1,2);
-else
-    for i=1:m
-        y = y+((-1) ^ (1+i)) * A(1,i) * cofator(A,1,i);
-end
-end
+    elseif (m == 2)
+        y = A(1,1) * A(2,2)-A(2,1) * A(1,2);
+    else
+        for i=1:m
+            y = y+((-1) ^ (1+i)) * A(1,i) * cofator(A,1,i);
+    end
+    end
 end
--- trabalho1/determinantes/det_by_prop.m
+++ trabalho1/determinantes/det_by_prop.m
@@ -1,22 +1,26 @@
 run("../sistemas_lineares/LU_pivot.m")
 
 function [Det] = det_by_prop(A)
-disp(A)
-m = length(A);
-y = 0;
-
-if size(A,1)~=size(A,2)
-    warn_msg = "essa nao eh uma matriz quadrada";
-    disp(warn_msg);
-
-elseif (m == 2)
-    y = A(1,1) * A(2,2)-A(2,1) * A(1,2);
-
-else
-    [L, U, P, cont_perm] = LU_pivot(A);
-    disp("U matrix: "), disp(U)
-    main_diag = diag(U);
-    disp("Main diagonal: ") , disp(main_diag)
-    Det = (-1)^cont_perm * (prod(main_diag));
-    disp("Determinant value: ") , disp(Det)
-end
+    disp(A)
+    m = length(A);
+    y = 0;
+    % checking if the matrix it's a square matrix, 
+    % if not a square matrix, display a message
+    if size(A,1)~=size(A,2)
+        warn_msg = "this is not a square matrix";
+        disp(warn_msg);
+    % checking if the matrix it's 2x2 and executing the calculation of determinant
+    elseif (m == 2)
+        y = A(1,1) * A(2,2)-A(2,1) * A(1,2);
+    % for others cases there not fit the two validations before, this piece of code
+    % apply the calculation of determinant based on Det(A) = (-1)^s*Det(U) formula;
+    % and this code is using the LU decomposition from "sistemas_lineares" module
+    else
+        [L, U, P, cont_perm] = LU_pivot(A);
+        disp("U matrix: "), disp(U)
+        main_diag = diag(U);
+        disp("Main diagonal: ") , disp(main_diag)
+        Det = (-1)^cont_perm * (prod(main_diag));
+        disp("Determinant value: ") , disp(Det)
+    end
+endfunction
